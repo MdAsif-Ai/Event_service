83cc8790fb96553ac88f0482e981ee590a34855c
MdAsif-Ai <md.asifmd46@gmail.com>
2026-08-12 10:39:31 +0530

    change image type

--- app/schemas/event.py
+++ app/schemas/event.py
@@ -1,15 +1,13 @@
-import base64
-import binascii
 from datetime import date, time
 
-from pydantic import BaseModel, Field, HttpUrl, field_validator
+from pydantic import BaseModel, Field, HttpUrl
 
 
 class EventCreate(BaseModel):
     title: str = Field(min_length=1)
     description: str
 
-    photos: list[str] = Field(default_factory=list)
+    photos: list[HttpUrl] = Field(default_factory=list)
 
     student_coordinators: list[str] = Field(default_factory=list)
     staff_coordinators: list[str] = Field(default_factory=list)
@@ -25,21 +23,6 @@ class EventCreate(BaseModel):
 
     registration_form_link: HttpUrl
 
-    @field_validator("photos")
-    @classmethod
-    def validate_photos(cls, photos: list[str]) -> list[str]:
-        for photo in photos:
-            try:
-                base64.b64decode(
-                    photo,
-                    validate=True,
-                )
-            except (ValueError, binascii.Error):
-                raise ValueError(
-                    "Each photo must be a valid Base64-encoded string"
-                )
-
-        return photos
 
 
 class EventResponse(EventCreate):
